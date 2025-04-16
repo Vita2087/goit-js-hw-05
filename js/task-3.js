6062ab8b757c52d2e9ab1acbbb88336fd364e246
Vita20 <vitahappy20@gmail.com>
2025-04-16 21:16:07 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,38 +1,85 @@
-// Задача 3. Профіль гравця
-// Об'єкт profile описує профіль користувача на ігровій платформі.
-// У його властивостях зберігається ім'я профілю username та кількість активних годин playTime,
-// проведених у грі.
+// Задача 3. Сортування за кількістю друзів
 
-// Доповни об'єкт profile методами для роботи з його властивостями.
+// Напиши стрілочну функцію sortByDescendingFriendCount(users),
+// яка прийматиме один параметр users — масив об'єктів користувачів.
 
-// Метод changeUsername(newName) повинен приймати рядок (нове ім'я)
-// в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Функція має повертати масив усіх користувачів, відсортованих
+// за спаданням кількостій їх друзів(властивість friends).
 
-// Метод updatePlayTime(hours) повинен приймати число (кількість годин)
-// у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
 
-// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!,
-// де <Username> — це ім'я профілю, а <amount> — кількість ігрових годин.
-
-const profile = {
-  username: "Jacob",
-  playTime: 300,
-    changeUsername(newName) {
-        this.username = newName;
-  },
-    updatePlayTime(hours) {
-        this.playTime += hours;
-  },
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-  },
-};
-
-// console.log(profile.changeUsername);
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername('Marco');
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
